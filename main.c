c01294aa610af896fcd847f51cdfecca638ee9f5
Reine Zeineddine <rhz06@mail.aub.edu>
2024-10-27 20:10:39 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,16 +5,34 @@
 #include "Grid_player.c"
 
 
+struct Input
+{
+    char orientation[11];
+    char moveName[10];
+
+    int column;
+    int row;
+    int valid;
+};
+
 #define GRID_SIZE 10
 #define EASY 0
 #define HARD 1
+#define INPUT struct Input
 
 void initializeGrid(char grid[GRID_SIZE][GRID_SIZE]);
-
 void assignStartingPlayer(char**,char**,char*,char*);
 int getDifficultyLevel();
 void getName(char*, int,int);
 void initializeGridHelper(char[GRID_SIZE][GRID_SIZE],char[GRID_SIZE][GRID_SIZE],char[GRID_SIZE][GRID_SIZE]);
+int getTrailingCharachterIndex(char*, int );
+int getLeadingCharachterIndex(char* , int );
+void getInput(struct Input*);
+
+
+
+
+
 
 int main()
 {
@@ -55,14 +73,115 @@ int main()
 
     while(shipsLeft1>0 && shipsLeft2>0)
     {
+        printGrid(hitsAndMissesGrid1);
+        printf("%s, what is your move?\n",firstPlayerName);
+        printf("for a list of moves, enter \"help\"\n");
 
+        
+        INPUT input;
+        getInput(&input);
+
+        printf("input move is : %s.",input.moveName);
+        if (strcasecmp(input.moveName, "fire") == 0)
+        {
+            printf("najah");
+            //fireMove(grid, playerName,gridHitsandMisses);
+        }
+        else if (strcasecmp(input.moveName, "radar") == 0)
+        {
+            // radarSweep(grid, playerName);
+        }
+        else if (strcasecmp(input.moveName, "smoke") == 0)
+        {
+            // smokeScreen(grid, playerName);
+        }
+        else if (strcasecmp(input.moveName, "artillery") == 0)
+        {
+            // artillery(grid, playerName);
+        }
+        else if (strcasecmp(input.moveName, "torpedo") == 0)
+        {
+            // torpedo(grid, playerName);
+        }
+        else
+        {
+            printf("Invalid input. Turn skipped.. \n");
     }
 
 
-    
-    
+
+    }
+
    
 }
+
+
+// int compareStrings(char*s1, char* s2)
+// {
+//    // compareStrings(s1,s2,0,sizeof(s1));
+// }
+
+// int compareStrings(char* s1, char* s2, int startIndex1,int endIndex1)
+// {
+//     for(int i = 0; i< endIndex1-startIndex1;i++)
+//     {
+//        if( s1[startIndex1+i] != s2[i])
+//        {
+//             return -1;
+//        }
+//     }
+
+//     return 0;
+// }
+
+int getLeadingCharachterIndex(char*input , int start)
+{
+    while(input[start]== ' ')
+    {
+        start++;
+    }
+
+    return start;
+}
+
+int getTrailingCharachterIndex(char*input, int start)
+{
+    while(input[start]!= ' ' && input[start]!= '\n'&& input[start]!= EOF )
+    {
+        start++;
+    }
+
+    return start;
+}
+
+void getInput(INPUT* input)
+{
+    char stringInput[20];
+
+    fgets(stringInput,sizeof(stringInput),stdin);
+    clearInput(stringInput,sizeof(stringInput));
+
+    int leading1 = getLeadingCharachterIndex(stringInput,0);
+    int trailing1 = getTrailingCharachterIndex(stringInput,leading1);
+
+    for(int i = 0; i < trailing1-leading1;i++)
+    {
+        input->moveName[i]= stringInput[leading1+i];
+    }
+
+    int leading2 = getLeadingCharachterIndex(stringInput, trailing1);
+    int trailing2 = getTrailingCharachterIndex(stringInput,leading2);
+   
+
+
+    int leading3 = getLeadingCharachterIndex(stringInput, leading2+1);
+    int trailing3 = getTrailingCharachterIndex(stringInput,leading3);
+    
+    input->column= getColumn(stringInput[leading2]);
+    input->row = getRow(stringInput, leading3, trailing3);
+
+}
+
 
 void assignStartingPlayer(char** firstPlayerName, char** secondPlayerName, char*name1, char*name2)
 {
