df42be9ce6eb95b0cbb164beee742449201c3081
Reine Zeineddine <rhz06@mail.aub.edu>
2024-10-29 21:48:55 +0200

    added comments

--- ArtilleryMove.c
+++ ArtilleryMove.c
@@ -1,16 +1,17 @@
 
 #include "headerFile.h"
-
+//functio to perfom an artillery attack on a 2x2 area
 void artilleryMove(PLAYER* currentPlayer, PLAYER* opposingPlayer,INPUT input)
 {
+    //check if the move is allowed, it requiered at least one ship on the sunk
     if (opposingPlayer->shipsLeft > 4) { // i can only access this move if at least 1 ship is sunk
         printf("Artillery conditions are not met.\n");
         return;
     }
-    //check the validation of the input
+    //define the starting point from the input
     int startrow = input.row-1;
     int startcol = input.column;
-    
+    //check the validation of the input
     if (startrow < 0 || startrow >= GRID_SIZE || startcol < 0 || startcol >= GRID_SIZE)
     {
         printf("Invalid coordinates");
