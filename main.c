de3da28d9b5b8745472d2e98066cad9c994432b7
Lea-Mourad <leamurad20@gmail.com>
2024-10-29 17:54:57 +0200

    Update main.c
    Added needed headers for Artillery Move and Fixed the "Help" option to stay on the same player's turn.

--- main.c
+++ main.c
@@ -9,44 +9,48 @@
 #include "smoke_screen.c"
 #include "RadarSweep.c"
 #include "torpedo.c"
-
-
+#include "ArtilleryMove.c"
 
 int main()
-{  
+{
 
     PLAYER player1;
     PLAYER player2;
 
     int difficultyLevel = getDifficultyLevel();
-    
-    initializePlayer(&player1,1);
-    initializePlayer(&player2,2);
 
+    initializePlayer(&player1, 1);
+    initializePlayer(&player2, 2);
 
-    assignStartingPlayer(&player1,&player2);
-    
+    assignStartingPlayer(&player1, &player2);
 
     placeShips(&player1);
     placeShips(&player2);
 
-    for(int i=0; player1.shipsLeft>0 && player2.shipsLeft>0 ;i++)
+   for (int i = 0; player1.shipsLeft > 0 && player2.shipsLeft > 0;)
     {
-        if(i%2==0)
+        // Check if the current turn results in a successful move
+        int turnSuccess;
+        if (i % 2 == 0)
         {
-            game(&player1,&player2);
-        }else
+            turnSuccess = game(&player1, &player2);
+        }
+        else
         {
-            game(&player2,&player1);
+            turnSuccess = game(&player2, &player1);
+        }
+
+        // Only increment i if the turn was successful (when i dont enter help)
+        if (turnSuccess) {
+            i++;
         }
     }
+    return 0;
 }
 
-
-
-void game(PLAYER* currentPlayer, PLAYER* opposingPlayer)
+int game(PLAYER *currentPlayer, PLAYER *opposingPlayer)
 {
-    printf("%s, what is your move?\n",currentPlayer->name);
+    printf("%s, what is your move?\n", currentPlayer->name);
     printf("for a list of moves, enter \"help\"\n");
 
     printGrid(currentPlayer->hitsAndMissesGrid);
@@ -54,35 +58,47 @@ void game(PLAYER* currentPlayer, PLAYER* opposingPlayer)
     getInput(&input);
     input.row--;
 
-
-
     if (strcasecmp(input.moveName, "fire") == 0)
     {
-        fireMove(currentPlayer,opposingPlayer,input);
+        fireMove(currentPlayer, opposingPlayer, input);
+        return 1; // Successful turn
     }
     else if (strcasecmp(input.moveName, "radar") == 0)
     {
-        RadarSweep(currentPlayer,opposingPlayer,&input);
+        RadarSweep(currentPlayer, opposingPlayer, &input);
+        return 1; // Successful turn
     }
     else if (strcasecmp(input.moveName, "smoke") == 0)
     {
-        smokeScreen(currentPlayer,opposingPlayer,input);
-        
+        smokeScreen(currentPlayer, opposingPlayer, input);
+        return 1; // Successful turn
     }
     else if (strcasecmp(input.moveName, "artillery") == 0)
-    {
-        // artillery(grid, playerName);
+    { 
+     
+        artilleryMove(currentPlayer, opposingPlayer, input);
+        
+
+        return 1; // Successful turn
     }
     else if (strcasecmp(input.moveName, "torpedo") == 0)
     {
-        torpedo(currentPlayer,opposingPlayer,&input);
+        torpedo(currentPlayer, opposingPlayer, &input);
+        return 1; // Successful turn
+    }
+    else if (strcasecmp(input.moveName, "help") == 0)
+    {
+        printf("Moves are:\n");
+        printf("1) Fire.\n");
+        printf("2) Radar Sweep(you are only allowed 3 radar sweeps).\n");
+        printf("3) Smoke Screen(you are allowed one smoke screen per ship they have sunk).\n");
+        printf("4) Artillery(can be accessed after sinking opponent's ship ).\n");
+        printf("5) Torpedo(can be accessed after sinking opponent's third ship).\n");
+        return 0; // "Help" does not end the turn
     }
     else
     {
-        printf("Invalid input. Turn skipped.. \n");
+        printf("Invalid input. Turn skipped.\n");
+        return 1; 
     }
-    
 }
-
-
-
