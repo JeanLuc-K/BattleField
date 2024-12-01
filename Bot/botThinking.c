f22d0a0ab19f6f8dd30b2c2419188dbbef4c6877
Lea Mourad <leamurad20@gmail.com>
2024-12-01 18:15:45 +0200

    Update botThinking.c
    added specifications

--- Bot/botThinking.c
+++ Bot/botThinking.c
@@ -1,4 +1,14 @@
 #include "../headerFile.h"
+*/
+requires: a reference to currentPlayer when current player is bot
+          a reference to opposingPlayer 
+          
+effects:  determines the bot's next move based on the current state of the game.
+          the bot will first attempt to use a radar sweep, then a torpedo if available, followed by artillery if applicable.
+          if no special actions are available, the bot will perform a regular attack.
+*/
+          
+
 
 void botThinking(PLAYER *currentPlayer, PLAYER *opposingPlayer)
 {
