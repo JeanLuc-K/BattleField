9c992e8e8e1fa4ad0539575cac3d6b1477571475
Lea Mourad <leamurad20@gmail.com>
2024-12-01 18:54:44 +0200

    Update placeShipsBot.c
    added specs

--- Bot/placeShipsBot.c
+++ Bot/placeShipsBot.c
@@ -1,5 +1,14 @@
 #include "headerFile.h"
+*/
+requires: a reference to currentPlayer hip name.
+         Size of the ship.
+         row, col ccordinates which are the starting position for the ship.
+         Ship orientation (HORIZONTAL or VERTICAL).
+          
+effects: Marks the grid with 'X' for the ship's location.
+         Updates the player's ship information.
 
+*/
 void addShip(PLAYER* currentPlayer, const char* name,int shipSize, int row, int col, int orientation)
 {   
     SHIP* ships = currentPlayer->ships;
@@ -38,6 +47,15 @@ void addShip(PLAYER* currentPlayer, const char* name,int shipSize, int row, int 
     ships[shipIndex].hasFallen =0;
 
 }
+    */
+requires: a reference to currentPlayer hip name.
+         Size of the ship.
+         row, col ccordinates which are the starting position for the ship.
+         Ship orientation (HORIZONTAL or VERTICAL).
+Effects:
+Returns 1 if valid placement, -1 if out-of-bounds or overlaps.
+
+*/
 
 int checkBounds(PLAYER* currentPlayer,int row, int col,int shipSize, int orientation)
 {
@@ -120,7 +138,15 @@ void placeShipsEasy(PLAYER* player) //edit it for bot
     printGrid(player->grid);
     printf("\n");
 }
-
+/*
+requires: A reference to the player object (player) to place ships on their grid.
+          The game mode to be medium
+          
+effects:  Randomly places ships on the player's grid based on the following :
+          Ships are not placed adjacent to each other.
+          Updates the player's grid and ship placement information.
+          Prints the placement of each ship along with its coordinates and orientation.
+*/
 void placeShipsMedium(PLAYER* player)
 //i want to implement for the bot to place the ships randomly but do not put them next to each others
 {
@@ -152,6 +178,13 @@ void placeShipsMedium(PLAYER* player)
     printGrid(player->grid);
     printf("\n");
 }
+    /*
+requires: A reference to the player object (player) containing the grid where ships are placed.
+          The starting coordinates of the ship (row, col), the ship's size, and its orientation
+
+effects: Returns 1 if any adjacent cells contain a ship ('X'), indicating the ship cannot be placed.
+         Returns 0 if no adjacent cells contain a ship, indicating the ship can be placed.
+*/
 int IsadjacentShips(PLAYER *player, int row, int col, int shipSize, int orientation){
     for(int i = 0, i < shipSize, i++){
         int r = row, c = col;
@@ -185,6 +218,16 @@ int IsadjacentShips(PLAYER *player, int row, int col, int shipSize, int orientat
     }
     return 0;
 }
+/*
+requires: A reference to the player object (player) to place ships on their grid.
+          The game mode to be hard
+          
+effects:   Randomly places ships on the player's grid based on the following :
+           ships are not placed too near the edges of the grid 
+           Ships are not placed in the corners unless corner placement is allowed 
+           Updates the player's grid and ship placement information.
+           Prints the placement of each ship along with its coordinates and orientation.
+*/
 
 void placeShipsHard(PLAYER* player){
     printf("%s, the bot is placing its ships.\n", player->name);
@@ -219,6 +262,13 @@ void placeShipsHard(PLAYER* player){
     printGrid(player->grid);
     printf("\n");
 }
+    /*
+requires: The starting coordinates of the ship (row, col), the ship's size, the orientation 
+          and a gap type (small or large) to determine the minimum distance from the edges.
+          
+effects: Returns 1 if the ship is placed too close to the edge of the grid based on the gap type.
+         Returns 0 otherwise.
+*/
 int isNearEdge(int row, int col, int shipSize, int orientation, int gapType)
 {
     // the gap between ships: small gap 0 vs large gap 1
@@ -231,6 +281,13 @@ int isNearEdge(int row, int col, int shipSize, int orientation, int gapType)
     }
     return 0; 
 }
+    /*
+requires: The starting coordinates of the ship (row, col), the ship's size, the orientation
+          and a flag indicating whether corner placement is allowed (allowCornerPlace).
+          
+effects: Returns 1 if the ship is placed in one of the grid's corners when corner placement is not allowed.
+         Returns 0 otherwise.
+*/
 int isInCorner(int row, int col, int shipSize, int orientation, int allowCornerPlace)
 {
     // If corner placement is not allowed, check if the ship is within the 5x5 corner area
