6bebab56490b95b0a8f83f9388133251b9e01b01
Reine Zeineddine <rhz06@mail.aub.edu>
2024-12-01 11:54:56 +0200

    added specifications

--- Moves/moveFire.c
+++ Moves/moveFire.c
@@ -1,5 +1,8 @@
 #include "../headerFile.h"
-
+/*void fireMove(PLAYER* currentPlayer, PLAYER* opposingPlayer,INPUT* input)
+requires: reference to the current player of type Player containing the hitsAndMissesGrid and the opposing player of type PLAYER containing the grid, input of type INPUT containing the coordinaes of the target to fire at 
+effects: returns if it is hit twice or once, or missed twice or once
+*/
 void fireMove(PLAYER* currentPlayer, PLAYER* opposingPlayer,INPUT* input)
 {
  
@@ -43,6 +46,10 @@ void fireMove(PLAYER* currentPlayer, PLAYER* opposingPlayer,INPUT* input)
 }
 
 //function to register a hit on the ship for the player
+/*void addHitOnShip(PLAYER* currentPlayer,PLAYER* opposingPlayer, INPUT* input)
+requires: reference to the current player who is firing the shot of type PLAYER, reference to the opposing player who is being attacked of type PLAYER, input of type INPUT containing the coordinates for the shot
+effects: increament the hit count for the shotten ship, and return if it has fallen
+*/
 void addHitOnShip(PLAYER* currentPlayer,PLAYER* opposingPlayer, INPUT* input)
 {
 
@@ -80,6 +87,10 @@ void addHitOnShip(PLAYER* currentPlayer,PLAYER* opposingPlayer, INPUT* input)
     }
 }
 
+/*void shipFallen(PLAYER* currentPlayer,PLAYER* opposingPlayer, INPUT* input)
+requires: reference to the current player who is firing the shot of type PLAYER, reference to the opposing player who is being attacked of type PLAYER, input of type INPUT containing the coordinates for the shot, the ship should be sunk
+effects: decremente the number of ships left, unlock artillery moves, and if the opponent has only 1 ship left, toperdo becomes available
+*/
 void shipFallen(PLAYER* currentPlayer,PLAYER* opposingPlayer, INPUT* input)
 {
     
