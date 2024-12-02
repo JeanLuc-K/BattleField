bafdc85872b2545527acc06d10733a2575581b8b
JeanLuc-K <jeanluc.kiami@gmail.com>
2024-12-02 06:41:30 +0200

    finalized bot thinking procccess and added difficulty

--- Bot/botThinking.c
+++ Bot/botThinking.c
@@ -12,52 +12,42 @@ effects:  determines the bot's next move based on the current state of the game.
 
 void botThinking(PLAYER *currentPlayer, PLAYER *opposingPlayer)
 {
-    currentPlayer->radarSweep=9;
-    currentPlayer->artillery=0;
-    currentPlayer->torpedo=0;
-
-    printf("\n\n THE PROBABILITY GRID IS: \n\n");
-    printGridInt(currentPlayer->probGrid);
+ 
     
     //first we do the radars
-
-    int radarResult = handleRadar(currentPlayer,opposingPlayer);
-    if(radarResult==1)
+    if(difficulty!=EASY)
     {
-        return; //done
+        int radarResult = handleRadar(currentPlayer,opposingPlayer);
+        if(radarResult==1)
+        {
+            return; //done
+        }
+
+
+        if(currentPlayer->torpedo==2)
+        {
+            
+            handleTorpedo(currentPlayer,opposingPlayer);
+            return; //always end and done
+            //do the torpedo first
+        }
+
+        if(currentPlayer->artillery ==2)
+        {
+            INPUT input;
+            handleArtillery(currentPlayer,opposingPlayer);
+            //handleArtillery(currentPlayer,opposingPlayer);
+            return; //always end
+
+            //do artillery
+        }
     }
     
-    
-    if(currentPlayer->torpedo==2)
-    {
-        /*
-        // INPUT input;
-        // input.row = 2;
-        // input.column =7;
-        // fireMove(currentPlayer,opposingPlayer, &input);
-        // calculateProbability(currentPlayer,opposingPlayer);
-        // printGridInt(currentPlayer->probGrid);
-
-        //handleTorpedo(currentPlayer,opposingPlayer);
-        */
-
-        handleTorpedo(currentPlayer,opposingPlayer);
-        return; //always end and done
-        //do the torpedo first
-    }
-    
-    if(currentPlayer->artillery ==2)
-    {
-        INPUT input;
-        handleArtillery(currentPlayer,opposingPlayer);
-        //handleArtillery(currentPlayer,opposingPlayer);
-        return; //always end
-
-        //do artillery
-    }
     
     INPUT input;
     handleFire(currentPlayer,opposingPlayer);
+
+    //smoke screen is useless 
 
      
 }
